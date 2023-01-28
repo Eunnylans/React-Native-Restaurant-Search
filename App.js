ab2b0a49a35bf15a183e88b6907b84587e30ca8e
Eunice Igbinedion <eunice.igbinedion@yahoo.com>
2023-01-28 21:26:13 +0100

    Updating App & CategoryItem

--- App.js
+++ App.js
@@ -1,23 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import CategoryItem from "./src/components/CategoryItem";
 import Header from "./src/components/Header";
 import Search from "./src/components/Search";
 
 export default function App() {
+  const [term, setTerm] = useState("Burger");
+
+  //FlatList: rendering multiple items
+  const commonCategories = [
+    { name: "Burger", imageUrl: require("../assets/images/burger.png") },
+    { name: "Cake", imageUrl: require("../assets/images/cake.png") },
+    { name: "Pasta", imageUrl: require("../assets/images/pasta.png") },
+    { name: "Pizza", imageUrl: require("../assets/images/pizza.png") },
+    { name: "Smoothies", imageUrl: require("../assets/images/smoothies.png") },
+    { name: "Steak", imageUrl: require("../assets/images/steak.png") },
+  ];
+
+  setTimeout(() => {
+    console.log("Initial Term:", term);
+    setTerm("Pizza");
+    console.log("After Change Term:", term);
+  }, 3000);
+
   return (
     <View style={styles.container}>
       <Header />
       <Search />
+      <FlatList
+        data={commonCategories}
+        renderItem={({ item, index }) => {
+          // console.log(item)
+          return (
+            <CategoryItem
+              name={item.name}
+              imageUrl={item.imageUrl}
+              index={index}
+              active={item.name === term}
+            />
+          );
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(category) => category.name}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
+  container: {},
 });
---
+++ src/components/CategoryItem.js
@@ -0,0 +1,56 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import { elevation } from "../common/styles";
+
+export default function CategoryItem({ name, imageUrl, index, active }) {
+  return (
+    <View
+      style={[
+        styles.container,
+        styles.elevation,
+        index === 0 ? { marginLeft: 25 } : { marginLeft: 15 },
+        active
+          ? { backgroundColor: "rgb(241, 186, 87)" }
+          : { backgroundColor: "white" },
+      ]}
+    >
+      <View style={styles.imageContainer}>
+        <Image source={imageUrl} style={styles.image} />
+      </View>
+      <Text style={styles.header}>{name}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 70,
+    height: 100,
+    border: 50,
+    marginVertical: 15,
+
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  elevation,
+
+  image: {
+    width: 35,
+    height: 35,
+  },
+
+  imageContainer: {
+    width: 50,
+    height: 50,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    marginBottom: 5,
+  },
+
+  header: {
+    fontWeight: "bold",
+  },
+});
